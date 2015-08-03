192e54d795065806adb84adb0c8ac0a4c68da9da
Brad McDavid <brad@wsol.com>
2015-08-03 09:43:21 -0400

    Added IsNullOrEmpty IContent extension. Updated packages. Updated Visual Studio Extensions.

---
+++ WSOL.EktronCms.ContentMaker/Extensions/IContentExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace WSOL.EktronCms.ContentMaker.Extensions
+{
+    using WSOL.EktronCms.ContentMaker.Interfaces;
+
+    public static class IContentExtensions
+    {
+        /// <summary>
+        /// Determines if IContent has an ID and HTML string
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this IContent content)
+        {
+            if (content != null)
+            {
+                if (content.Id != 0 && content.Html != null)
+                {
+                    string s = content.Html;
+
+                    for (int index = 0; index < s.Length; ++index)
+                    {
+                        if (!char.IsWhiteSpace(s[index]))
+                            return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
