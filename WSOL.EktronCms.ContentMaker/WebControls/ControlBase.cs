12dc0e811a61ca6947f7be86cf24945c6ab61ca2
Brad McDavid <brad@wsol.com>
2015-11-23 08:54:04 -0500

    Fixed deprecated message. Added solution folders for git and markdown files.

--- WSOL.EktronCms.ContentMaker/WebControls/ControlBase.cs
+++ WSOL.EktronCms.ContentMaker/WebControls/ControlBase.cs
@@ -1,6 +1,6 @@
 ﻿namespace WSOL.EktronCms.ContentMaker.WebControls
 {
-    [System.Obsolete("Please use WSOL.ObjectRenderer.ControlBase instead.")]
+    [System.Obsolete("Please use WSOL.ObjectRenderer.WebControls.ControlBase instead.")]
     public class ControlBase<T> : WSOL.ObjectRenderer.WebControls.ControlBase<T>
     {
         public ControlBase() : base() { }   
